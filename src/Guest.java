89d669351eafaa64f4d949b53e107545790f263f
Oleksii <oleksiispartan@gmail.com>
2021-01-23 18:13:58 -0500

    seckond commit

--- src/Guest.java
+++ src/Guest.java
@@ -1,3 +1,36 @@
 public class Guest {
-    public String name;
-}
+
+
+
+        public String firstName;
+        public String lastName;
+        public String phoneNumber;
+        public String email;
+        public String address;
+        public int plus;
+        public static int tCount;
+
+
+        public Guest (String firstName,String lastName, String email, String phoneNumber ) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+        }
+        public Guest() {
+        }
+
+        @Override
+        public String toString() {
+            return "Guest{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", phoneNumber='" + phoneNumber + '\'' +
+                    ", email='" + email + '\'' +
+                    ", address='" + address + '\'' +
+                    ", plus=" + plus +
+                    '}';
+        }
+    }
+
+
