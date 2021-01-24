f123ce0020aad0b0273cc144903bb90f04466f55
Oleksii <oleksiispartan@gmail.com>
2021-01-23 19:35:27 -0500

    music goes on

---
+++ src/Music.java
@@ -0,0 +1,22 @@
+public class Music {
+    public static void main(String[] args) {
+
+        String one = ("Michael Jackson The way you make me feel");
+        String two = ( "Bruno Mars: Uptown funk");
+        String three = ( "Pharrell: Happy");
+        String four = ( "Monatik: Spinning");
+        String five = ("Modern talking: We can win the race");
+        String six = ( "Four seasons: To good to be true.");
+        String seven = ("Spice girls: Wannabe");
+        String eight = (" Backstreet boys: Everybody");
+        String nine = ("Cali swag district: Teach me how to dougie");
+        String ten = ( " Queen: We will rock you");
+        String eleven = ( "Lil Nas X: Old town road");
+        String twelwe = ( " Rastmus and ...: Beter Sweet");
+        String thirten = ("");
+
+    }
+
+}
+
+}
